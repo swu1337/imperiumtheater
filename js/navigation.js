6c7b6bb1627dae7daeee4b978b4886e62d02c381
Steven Wu <master_msp1@hotmail.com>
2018-03-27 02:03:10 +0200

    finished header

--- js/navigation.js
+++ js/navigation.js
@@ -3,13 +3,18 @@ $(function() {
         var scrollTop = $(window).scrollTop();
         if(scrollTop > 0) {
             $('.stickyheader').addClass('inverse');
+            $('.logo').addClass('inverse');
             $('li > a.focus').addClass('inverse');
-            $('ul > li > a').addClass('inverse');
+            $('header nav').addClass('inverse');
+            $('svg#logo').addClass('inverse');
+            $('.st0, .st1').css('fill', 'white');
         } else {
             $('.stickyheader').removeClass('inverse');
+            $('.logo').removeClass('inverse');
             $('li > a.focus').removeClass('inverse');
-            $('ul > li > a').removeClass('inverse');
+            $('header nav').removeClass('inverse');
+            $('svg#logo').removeClass('inverse');
+            $('.st1').css('fill', '#DE134C');
         }
-
     });
 });
