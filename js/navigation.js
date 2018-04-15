c42bcba04a6d9bfbc29c816f24c73072ab41037c
Steven Wu <master_msp1@hotmail.com>
2018-04-15 23:00:47 +0200

    home and galery done

--- js/navigation.js
+++ js/navigation.js
@@ -1,20 +1,29 @@
 $(function() {
-    $(window).on('scroll', function() {
-        var scrollTop = $(window).scrollTop();
-        if(scrollTop > 0) {
-            $('.stickyheader').addClass('inverse');
-            $('.logo').addClass('inverse');
-            $('li > a.focus').addClass('inverse');
-            $('header nav').addClass('inverse');
-            $('svg#logo').addClass('inverse');
-            $('.st0, .st1').css('fill', 'white');
-        } else {
-            $('.stickyheader').removeClass('inverse');
-            $('.logo').removeClass('inverse');
-            $('li > a.focus').removeClass('inverse');
-            $('header nav').removeClass('inverse');
-            $('svg#logo').removeClass('inverse');
-            $('.st1').css('fill', '#DE134C');
-        }
-    });
+    if($('.landingpage').length) {
+        $(window).on('scroll', function() {
+            var scrollTop = $(window).scrollTop();
+            if(scrollTop > 0) {
+                $('.header-nav').addClass('inverse');
+                $('.logo').addClass('inverse');
+                $('li > a.focus').addClass('inverse');
+                $('header nav').addClass('inverse');
+                $('svg#logo').addClass('inverse');
+                $('.st0, .st1').css('fill', 'white');
+            } else {
+                $('.header-nav').removeClass('inverse');
+                $('.logo').removeClass('inverse');
+                $('li > a.focus').removeClass('inverse');
+                $('header nav').removeClass('inverse');
+                $('svg#logo').removeClass('inverse');
+                $('.st1').css('fill', '#DE134C');
+            }
+        });
+    } else {
+        $('.header-nav').addClass('inverse');
+        $('.logo').addClass('inverse');
+        $('li > a.focus').addClass('inverse');
+        $('header nav').addClass('inverse');
+        $('svg#logo').addClass('inverse');
+        $('.st0, .st1').css('fill', 'white');
+    }
 });
