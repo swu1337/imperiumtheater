f360a4344d92c1646af7612821e603a240bb7c42
swu1337 <master_msp1@hotmail.com>
2018-04-17 17:42:33 +0200

    localstorage for registration done

--- js/helper/localstorage.js
+++ js/helper/localstorage.js
@@ -1,6 +1,16 @@
 let setStorage = (index, data) => {
     localStorage.setItem(index, JSON.stringify(data));
 };
-let  getStorage = (index) => {
+let getStorage = (index) => {
     return JSON.parse(localStorage.getItem(index));
 };
+let objectFormatter = (array) => {
+    return {
+        vn : array[0],
+        tv : array[1],
+        an : array[2],
+        email : array[3],
+        tn : array[4],
+        ww: array[5]
+    }
+}
--- js/registration-validation.js
+++ js/registration-validation.js
@@ -3,10 +3,22 @@ $(function() {
             $('.register .action').on('click', function(e) {
                 e.preventDefault();
                 let input = [$('#vn').val(), $('#an').val(), $('#remail').val(), $('#tn').val(), $('#rpassword').val(), $('#hrpassword').val()];
+
                 if(checkInput(input)) {
-                    
+                    if(!emailExist($('#remail').val())) {
+                        if($('#rpassword').val() === $('#hrpassword').val()) {
+                            $('.register > .error').hide();
+                            let users = getStorage('users').users;
+                            input.splice(1, 0, $('#tv').val());
+                            users.push(objectFormatter(input));
+                            setStorage('users', users);
+                        } else {
+                            $('.register > .error').text('Wachtwoorden komen niet overeen!').show();
+                        }
+                    } else {
+                        $('.register > .error').text('Email is al in gebruik!').show();
+                    }
                 } else {
-                    console.log(222);
                     $('.register > .error').text('Vul alle invoervelden in!').show();
                 }
             });
